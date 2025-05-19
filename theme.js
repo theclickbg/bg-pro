6cab3a6bfa0be92f2e33ae97f3244f794deec5f7
theclickbg <yadavanjalee444@gmail.com>
2025-05-20 02:13:10 +0530

    Delete theme.js

--- theme.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var allowedDomain = "theclickbgmt.blogspot.com";
-  var currentDomain = window.location.hostname;
-
-  if (!currentDomain.includes(allowedDomain)) {
-    alert("⚠️ Warning: This theme is licensed for use only on " + allowedDomain);
-    window.location.href = "#";
-  }
-})();
